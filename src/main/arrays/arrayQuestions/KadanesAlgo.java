82a0d5d91a13453cfcb50d5f0c26960b9a0fe9d9
IamVk009 <imvk009@gmail.com>
2025-01-07 21:28:30 +0530

    Kadane's Algorithm : Most Optimized Solution

--- src/main/arrays/arrayQuestions/KadanesAlgo.java
+++ src/main/arrays/arrayQuestions/KadanesAlgo.java
@@ -3,11 +3,14 @@ package main.arrays.arrayQuestions;
 /** Maximum Subarray Sum. */
 public class KadanesAlgo {
     public static void main(String[] args) {
+        //  Given an array of integers, find the contiguous subarray that has the largest sum and return that sum.
         int[] arr = {-5, 4, 6, -3, 4, -1};
-        System.out.printf("Maximum Subarray Sum = " + maxSubarraySum(arr));
+        int[] arr1 = {-5, -3, -1};
+        System.out.println("Maximum Subarray Sum = " + kadanesAlgo(arr));
+        System.out.println("Maximum Subarray Sum = " + kadanesAlgo(arr1));
     }
 
-//    Approach - 1 : Brute force Approach
+    //  Approach - 1 : Brute force Approach
     static int maxSubarraySum(int[] arr) {
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
@@ -22,6 +25,39 @@ public class KadanesAlgo {
             }
         }
         return maxSum;
-     // Time Complexity = O(n^3)
+        // Time Complexity = O(n^3)
+    }
+
+    //   Approach - 2 : Brute force Approach
+    static int maxSubarraySum2(int[] arr) {
+        int maxSum = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+        // Time Complexity = O(n^2)
+    }
+
+    //  Approach - 3 : Most Optimized Solution
+    static int kadanesAlgo(int[] arr) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+        // Time Complexity = O(n)
     }
 }
