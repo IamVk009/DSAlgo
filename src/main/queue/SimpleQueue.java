a31197d13628ef5852867b0cbb7c5b53dbd8a5a0
IamVk009 <imvk009@gmail.com>
2025-01-17 23:21:13 +0530

    Simple Queue : Dequeue Operation

--- src/main/queue/SimpleQueue.java
+++ src/main/queue/SimpleQueue.java
@@ -24,4 +24,14 @@ public class SimpleQueue {
         queue[rear] = data;
         System.out.println(data + " is inserted in queue.");
     }
+
+    public int dequeue() {
+        if (front > rear) {
+            System.out.println("Queue is empty..!");
+            return -1;
+        }
+        int data = queue[front];
+        front++;
+        return data;
+    }
 }
